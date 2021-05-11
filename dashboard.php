da4f408a25610dc80eb9f274963e9ff8f11cb270
Gregor Zeman <if19b210@technikum-wien.at>
2021-05-11 17:56:34 +0200

    hier oida

---
+++ dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+if (! empty($_SESSION["userId"])) {
+    require_once __DIR__ . '/Users.php';
+    $member = new Users();
+    $memberResult = $member->getMemberById($_SESSION["userId"]);
+    if(!empty($memberResult[0]["display_name"])) {
+        $displayName = ucwords($memberResult[0]["display_name"]);
+    } else {
+        $displayName = $memberResult[0]["user_name"];
+    }
+}
+?>
+<html>
+<head>
+<title>User Login</title>
+<link href="./view/css/style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <div>
+        <div class="dashboard">
+            <div class="member-dashboard">Welcome <b><?php echo $displayName; ?></b>, You have successfully logged in!<br>
+                Click to <a href="./logout.php" class="logout-button">Logout</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
